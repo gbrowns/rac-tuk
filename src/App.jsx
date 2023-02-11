0b49bac38c57b82cfd466c66fb8f961d7fc90ecf
Godwin254 <godwinbee15@gmail.com>
2023-02-11 13:52:20 +0300

    add and configure tailwindcss rules

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      <h1>Rotaract Club of TUK</h1>
-      <span>Site Under development</span>
-      <a href='https://github.com/Godwin254/rac-tuk'> Visit Github Repo</a>
+    <div className="App flex flex-col justify-center items-center h-screen">
+      <h1 className="text-7xl pb-5 bg-rose-700 py-3 px-4 rounded-md ">Rotaract Club of TUK</h1>
+      <span className='text-2xl py-2'>Site Under development</span>
+      <a href='https://github.com/Godwin254/rac-tuk' className='text-lg underline'> Visit Github Repo</a>
     </div>
   )
 }
