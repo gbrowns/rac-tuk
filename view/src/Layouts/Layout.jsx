8a2aaefeb40b4988ca5abd61ebc77696696b226d
Gbrown <45181428+gbrowns@users.noreply.github.com>
2023-06-04 00:11:41 +0300

    Feat: Add icon for page link

--- view/src/Layouts/Layout.jsx
+++ view/src/Layouts/Layout.jsx
@@ -4,14 +4,13 @@ import { Footer, Navbar } from '../components'
 
 export const Layout = ({children}) => {
   return (
+    //layout to be modified base on screen size
     <div className='flex flex-col'>
       <Navbar />
       <main className=''>
         {children}
       </main>
-
       <Footer/>
-
     </div>
   )
 }
